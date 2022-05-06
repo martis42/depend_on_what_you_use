f82a95a2112722dd587501c6d622742151083f3f
Martin Medler <medler.martin@gmail.com>
2022-05-06 19:04:19 +0200

    Generalize test script for automatic fixes
    There will be more test cases. Thus, we require a script which is able to
    execute multiple tests and summarize the result.

--- test/apply_fixes/execute_tests.py
+++ test/apply_fixes/execute_tests.py
@@ -1,91 +1,26 @@
 #!/usr/bin/env python3
-
-import subprocess
 import sys
-import tempfile
-from distutils.dir_util import copy_tree
-from pathlib import Path
+from argparse import ArgumentParser
 
-WORKSPACE_TEMPLATE = """
-local_repository(
-    name = "depend_on_what_you_use",
-    path = "{dwyu_repo}",
-)
+from execute_tests_impl import TestCase, main
 
-load("@depend_on_what_you_use//:dependencies.bzl", dwyu_public_dependencies = "public_dependencies")
-
-dwyu_public_dependencies()
-"""
-
-# TODO generalize impl
+TESTS = [
+    TestCase(
+        name="remove_unused_dependency",
+        path="test/apply_fixes/unused_dependencies",
+        target="//:main",
+        expected_deps=["//:used"],
+    )
+]
 
 
-def get_current_workspace() -> Path:
-    process = subprocess.run(["bazel", "info", "workspace"], check=True, capture_output=True, text=True)
-    return Path(process.stdout.strip())
-
-
-def main():
-    success = True
-    with tempfile.TemporaryDirectory() as test_workspace:
-        try:
-            current_workspace = get_current_workspace()
-            test_sources = current_workspace / "test/apply_fixes/unused_dependencies"
-
-            # Setup test workspace
-            print(f"INFO: Created temporary directory for testing unused dependency removal: '{test_workspace}'")
-            copy_tree(src=test_sources, dst=test_workspace)
-            with open(Path(test_workspace) / "WORKSPACE", mode="wt", encoding="utf-8") as ws_file:
-                ws_file.write(WORKSPACE_TEMPLATE.format(dwyu_repo=current_workspace))
-
-            # create report file for unused dependency by running DWYU
-            subprocess.run(
-                [
-                    "bazel",
-                    "build",
-                    "//:main",
-                    "--aspects=//:aspect.bzl%dwyu_default_aspect",
-                    "--output_groups=cc_dwyu_output",
-                ],
-                cwd=test_workspace,
-                check=False,
-            )
-
-            # Perform automatic fix
-            subprocess.run(
-                ["bazel", "run", "@depend_on_what_you_use//:apply_fixes", "--", f"--workspace={test_workspace}"],
-                check=True,
-            )
-
-            # Make sure the unused dependency has been removed
-            process = subprocess.run(
-                ["bazel", "query", "labels(deps, //:main)"],
-                cwd=test_workspace,
-                check=True,
-                capture_output=True,
-                text=True,
-            )
-            deps_after_fix = {dep for dep in process.stdout.split("\n") if dep}
-            expected_deps = {"//:used"}
-            if expected_deps != deps_after_fix:
-                success = False
-                print(f"FAILED due to unexpected dependencies: {deps_after_fix}")
-                print(f"       expected: {expected_deps}")
-
-        # pylint: disable=broad-except
-        except Exception as ex:
-            success = False
-            print(f"FAILED due to excpetion: {ex}")
-
-        # Make sure the bazel cache dir is no swamped with dead test workspaces
-        subprocess.run(["bazel", "clean", "--expunge"], check=True)
-
-    if success:
-        print("Testing automatic fixes: SUCCESS")
-        return 0
-    print("Testing automatic fixes: FAILURE")
-    return 1
+def cli():
+    parser = ArgumentParser()
+    parser.add_argument("--verbose", "-v", action="store_true", help="Show output of test runs.")
+    parser.add_argument("--test", "-t", nargs="+", help="Run the specified test cases.")
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(main(args=cli(), test_cases=TESTS))
