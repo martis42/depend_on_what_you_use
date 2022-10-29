f3e1f94c34eace95bb6b0a88b11946712ecf78f5
Martin Medler <medler.martin@gmail.com>
2022-10-29 23:34:51 +0200

    Apply fixes acceptance tests utilize logging
    Instead of manually tracking a verbosity flag and using if else logic
    we utilize the logging framework for leaner and more pythonic code.

--- test/apply_fixes/src/test_case.py
+++ test/apply_fixes/src/test_case.py
@@ -1,3 +1,4 @@
+import logging
 import subprocess
 from abc import ABC, abstractmethod
 from pathlib import Path
@@ -10,7 +11,6 @@ class TestCaseBase(ABC):
     def __init__(self, name: str) -> None:
         self._name = name
         self._workspace = None
-        self._verbose = None
 
     #
     # Interface
@@ -39,9 +39,8 @@ class TestCaseBase(ABC):
     def name(self) -> str:
         return self._name
 
-    def execute_test(self, workspace: Path, verbose: bool = False) -> Result:
+    def execute_test(self, workspace: Path) -> Result:
         self._workspace = workspace
-        self._verbose = verbose
         return self.execute_test_logic()
 
     def _create_reports(self, startup_args: Optional[List[str]] = None, extra_args: Optional[List[str]] = None) -> None:
@@ -55,11 +54,7 @@ class TestCaseBase(ABC):
         if extra_args:
             cmd.extend(extra_args)
         cmd.append(self.test_target)
-        # Detecting the problems which we want to fix causes a red build, thus don't check results
-        if self._verbose:
-            subprocess.run(cmd, cwd=self._workspace, check=False)
-        else:
-            subprocess.run(cmd, cwd=str(self._workspace), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=False)
+        self._run_cmd(cmd=cmd, check=False)
 
     def _run_automatic_fix(self, extra_args: List[str] = None) -> None:
         """
@@ -68,23 +63,26 @@ class TestCaseBase(ABC):
         cmd = ["bazel", "run", "@depend_on_what_you_use//:apply_fixes", "--", f"--workspace={self._workspace}"]
         if extra_args:
             cmd.extend(extra_args)
-        if self._verbose:
-            subprocess.run(cmd, check=True)
-        else:
-            subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        self._run_cmd(cmd=cmd, check=True)
 
     def _get_target_attribute(self, target: str, attribute: str) -> Set["str"]:
         """
         Returns a set to ensure ordering is no issue
         """
-        process = subprocess.run(
-            ["bazel", "query", f"labels({attribute}, {target})"],
-            cwd=self._workspace,
-            check=True,
-            encoding="utf-8",
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-        if self._verbose:
-            print(process.stdout)
+        process = self._run_and_capture_cmd(cmd=["bazel", "query", f"labels({attribute}, {target})"], check=True)
         return {dep for dep in process.stdout.split("\n") if dep}
+
+    def _run_cmd(self, cmd: List[str], **kwargs) -> None:
+        logging.debug(f"Executing command: {cmd}")
+        if logging.getLogger().isEnabledFor(logging.DEBUG):
+            subprocess.run(cmd, cwd=self._workspace, **kwargs)
+        else:
+            subprocess.run(cmd, cwd=self._workspace, **kwargs, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    def _run_and_capture_cmd(self, cmd: List[str], **kwargs) -> subprocess.CompletedProcess:
+        logging.debug(f"Executing command: {cmd}")
+        process = subprocess.run(
+            cmd, cwd=self._workspace, **kwargs, encoding="utf-8", stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        logging.debug(process.stdout)
+        return process
