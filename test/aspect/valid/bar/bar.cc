a1ee2029b5c671c1f0e21caae6c6e4718dfe983f
Jiaming Liu <storypku@gmail.com>
2023-01-10 10:20:15 +0800

    Test: make include path relative to the workspace directory

--- test/aspect/valid/bar/bar.cc
+++ test/aspect/valid/bar/bar.cc
@@ -1,6 +1,7 @@
-#include "private_a.h"
-#include "sub/dir/private_b.h"
 #include "test/aspect/valid/bar/bar.h"
+
+#include "test/aspect/valid/bar/private_a.h"
+#include "test/aspect/valid/bar/sub/dir/private_b.h"
 #include "test/aspect/valid/foo/b.h"
 #include "test/aspect/valid/foo/textual.cc"
 
