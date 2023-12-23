b786508fa8b721b50e1c5f84373f03c2d022678c
Martin Medler <medler.martin@gmail.com>
2023-12-23 14:32:39 +0100

    Rename the DWYU output group
    There is no strong reason for this change. I am however not happy with
    the name chosen by me in the early project phase. Since, version 0.1.0
    will soonish happen (aka jumping from alpha status to beta level)
    I want to do this breaking change now, before the project enters a
    phase in which such changes no longer should happen without strong reasons.
    Some concrete issues with the old name:
    - The `cc_` prefix does not provide any meaningful information
    - The `_output` postfix is redundant for something used with `--output_groups`

--- test/aspect/execute_tests_utest.py
+++ test/aspect/execute_tests_utest.py
@@ -190,7 +190,7 @@ class TestMakeCmd(unittest.TestCase):
         self.assertEqual(
             cmd,
             self._base_cmd()
-            + ["--aspects=//some/aspect.bzl", "--output_groups=cc_dwyu_output", "--abc", "--cba", "--", "//foo:bar"],
+            + ["--aspects=//some/aspect.bzl", "--output_groups=dwyu", "--abc", "--cba", "--", "//foo:bar"],
         )
 
     def test_extra_args_on_top_of_test_cmd(self):
@@ -208,7 +208,7 @@ class TestMakeCmd(unittest.TestCase):
             self._base_cmd(startup_args=["--some_startup_arg"])
             + [
                 "--aspects=//some/aspect.bzl",
-                "--output_groups=cc_dwyu_output",
+                "--output_groups=dwyu",
                 "--outside_arg",
                 "--test_related_arg",
                 "--",
