82683a4cd3217e7c87a88758a5ae96496acc61b5
Martin Medler <medler.martin@gmail.com>
2023-12-28 11:16:51 +0100

    Add transitive_deps aspect tests

---
+++ test/aspect/using_transitive_dep/test_detect_using_transitive_impl_dep.py
@@ -0,0 +1,19 @@
+from result import ExpectedResult, Result
+from test_case import TestCaseBase
+
+
+class TestCase(TestCaseBase):
+    def execute_test_logic(self) -> Result:
+        expected = ExpectedResult(
+            success=False,
+            invalid_includes=[
+                "File='test/aspect/using_transitive_dep/transitive_usage_through_impl_deps.h', include='test/aspect/using_transitive_dep/foo.h'"
+            ],
+        )
+        actual = self._run_dwyu(
+            target="//test/aspect/using_transitive_dep:transitive_usage_through_impl_deps",
+            aspect="//test/aspect/using_transitive_dep:aspect.bzl%implementation_deps_aspect",
+            extra_args=["--experimental_cc_implementation_deps"],
+        )
+
+        return self._check_result(actual=actual, expected=expected)
---
+++ test/aspect/using_transitive_dep/transitive_usage_through_impl_deps.h
@@ -0,0 +1,9 @@
+#include "test/aspect/using_transitive_dep/foo.h"
+#include "test/aspect/using_transitive_dep/bar.h"
+
+int doSth() {
+    // ERROR: Using a function from library foo but depending only on library bar
+    const int answer = theAnswer();
+    const int stuff = doStuff();
+    return answer != stuff;
+}
