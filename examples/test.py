ded6d35e0035e2c40cca65d8c619b540558d70c4
Martin Medler <medler.martin@gmail.com>
2024-01-03 19:46:36 +0100

    Test the examples with bzlmod as well with the legacy setup
    We need to provide targets from the root workspace to DWYU explicitly with
    labels as auto generating labels from strings does not work with bzlmod.
    With bzlmod the strings are converted in the wrong context to a label and
    thus cause visibility errors.

--- examples/test.py
+++ examples/test.py
@@ -4,6 +4,7 @@ import logging
 import shlex
 import subprocess
 import sys
+from argparse import ArgumentParser
 from dataclasses import dataclass
 
 logging.basicConfig(format="%(message)s", level=logging.INFO)
@@ -11,65 +12,65 @@ logging.basicConfig(format="%(message)s", level=logging.INFO)
 
 @dataclass
 class Example:
-    cmd: str
+    build_cmd: str
     expected_success: bool
 
 
 EXAMPLES = [
     Example(
-        cmd="bazel build --aspects=//:aspect.bzl%dwyu --output_groups=dwyu //basic_usage:correct_dependencies",
+        build_cmd="--aspects=//:aspect.bzl%dwyu --output_groups=dwyu //basic_usage:correct_dependencies",
         expected_success=True,
     ),
     Example(
-        cmd="bazel build --aspects=//:aspect.bzl%dwyu --output_groups=dwyu //basic_usage:false_dependencies",
+        build_cmd="--aspects=//:aspect.bzl%dwyu --output_groups=dwyu //basic_usage:false_dependencies",
         expected_success=False,
     ),
     Example(
-        cmd="bazel build --aspects=//:aspect.bzl%dwyu --output_groups=dwyu //basic_usage:not_using_lib",
+        build_cmd="--aspects=//:aspect.bzl%dwyu --output_groups=dwyu //basic_usage:not_using_lib",
         expected_success=False,
     ),
     Example(
-        cmd="bazel build --aspects=//:aspect.bzl%dwyu_ignoring_includes --output_groups=dwyu //ignoring_includes:use_unavailable_headers",
+        build_cmd="--aspects=//:aspect.bzl%dwyu_ignoring_includes --output_groups=dwyu //ignoring_includes:use_unavailable_headers",
         expected_success=True,
     ),
     Example(
-        cmd="bazel build --aspects=//:aspect.bzl%dwyu --output_groups=dwyu //recursion:use_lib",
+        build_cmd="--aspects=//:aspect.bzl%dwyu --output_groups=dwyu //recursion:use_lib",
         expected_success=True,
     ),
     Example(
-        cmd="bazel build --aspects=//:aspect.bzl%dwyu_recursive --output_groups=dwyu //recursion:use_lib",
+        build_cmd="--aspects=//:aspect.bzl%dwyu_recursive --output_groups=dwyu //recursion:use_lib",
         expected_success=False,
     ),
     Example(
-        cmd="bazel build //rule_using_dwyu/...",
+        build_cmd="//rule_using_dwyu/...",
         expected_success=False,
     ),
     Example(
-        cmd="bazel build --aspects=//:aspect.bzl%dwyu --output_groups=dwyu //skipping_targets:bad_target",
+        build_cmd="--aspects=//:aspect.bzl%dwyu --output_groups=dwyu //skipping_targets:bad_target",
         expected_success=False,
     ),
     Example(
-        cmd="bazel build --aspects=//:aspect.bzl%dwyu --output_groups=dwyu //skipping_targets:bad_target_skipped",
+        build_cmd="--aspects=//:aspect.bzl%dwyu --output_groups=dwyu //skipping_targets:bad_target_skipped",
         expected_success=True,
     ),
     Example(
-        cmd="bazel build --aspects=//:aspect.bzl%dwyu_custom_skipping --output_groups=dwyu //skipping_targets:bad_target_custom_skip",
+        build_cmd="--aspects=//:aspect.bzl%dwyu_custom_skipping --output_groups=dwyu //skipping_targets:bad_target_custom_skip",
         expected_success=True,
     ),
     Example(
-        cmd="bazel build --aspects=//:aspect.bzl%dwyu_map_specific_deps --output_groups=dwyu //target_mapping:use_lib_b",
+        build_cmd="--aspects=//:aspect.bzl%dwyu_map_specific_deps --output_groups=dwyu //target_mapping:use_lib_b",
         expected_success=True,
     ),
     Example(
-        cmd="bazel build --aspects=//:aspect.bzl%dwyu_map_direct_deps --output_groups=dwyu //target_mapping:use_lib_b",
+        build_cmd="--aspects=//:aspect.bzl%dwyu_map_direct_deps --output_groups=dwyu //target_mapping:use_lib_b",
         expected_success=True,
     ),
     Example(
-        cmd="bazel build --aspects=//:aspect.bzl%dwyu_map_direct_deps --output_groups=dwyu //target_mapping:use_lib_c",
+        build_cmd="--aspects=//:aspect.bzl%dwyu_map_direct_deps --output_groups=dwyu //target_mapping:use_lib_c",
         expected_success=False,
     ),
     Example(
-        cmd="bazel build --aspects=//:aspect.bzl%dwyu_map_transitive_deps --output_groups=dwyu //target_mapping:use_lib_c",
+        build_cmd="--aspects=//:aspect.bzl%dwyu_map_transitive_deps --output_groups=dwyu //target_mapping:use_lib_c",
         expected_success=True,
     ),
 ]
@@ -81,25 +82,45 @@ class Result:
     success: bool
 
 
-def execute_example(example: Example) -> Result:
-    logging.info(f"\n##\n## Executing: {example.cmd}\n##\n")
+def make_cmd(example: Example, legacy_workspace: bool) -> str:
+    cmd = "bazel build "
+    if legacy_workspace:
+        cmd += "--noenable_bzlmod "
+    cmd += example.build_cmd
+    return cmd
 
-    process = subprocess.run(shlex.split(example.cmd), check=False)
+
+def execute_example(example: Example, legacy_workspace: bool) -> Result:
+    cmd = make_cmd(example=example, legacy_workspace=legacy_workspace)
+    logging.info(f"\n##\n## Executing: {cmd}\n##\n")
+
+    process = subprocess.run(shlex.split(cmd), check=False)
     if (process.returncode == 0 and example.expected_success) or (
         process.returncode != 0 and not example.expected_success
     ):
-        return Result(example=example.cmd, success=True)
-    return Result(example=example.cmd, success=False)
+        return Result(example=cmd, success=True)
+    return Result(example=cmd, success=False)
 
 
-def main() -> int:
+def cli():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--legacy-workspace",
+        "-w",
+        action="store_true",
+        help="Use the WORKSPACE file based project setup instead of bzlmod.",
+    )
+    return parser.parse_args()
+
+
+def main(legacy_workspace: bool) -> int:
     """
     Basic testing if the examples behave as desired.
 
     We ony look for the return code of a command. If a command fails as expected we do not analyze if it fails for the
     correct reason. These kind of detailed testing is done in the integration tests.
     """
-    results = [execute_example(ex) for ex in EXAMPLES]
+    results = [execute_example(example=ex, legacy_workspace=legacy_workspace) for ex in EXAMPLES]
     failed_examples = [res.example for res in results if not res.success]
 
     if failed_examples:
@@ -112,4 +133,5 @@ def main() -> int:
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    args = cli()
+    sys.exit(main(args.legacy_workspace))
