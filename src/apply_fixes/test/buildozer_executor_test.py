22b105e04463490310529e585b5abc5c09f7c717
Martin Medler <medler.martin@gmail.com>
2024-01-06 18:20:00 +0100

    Ruff: Allow accessing private class members in tests

--- src/apply_fixes/test/buildozer_executor_test.py
+++ src/apply_fixes/test/buildozer_executor_test.py
@@ -7,11 +7,11 @@ from src.apply_fixes.buildozer_executor import BuildozerExecutor
 class TestBuildozerExecutor(unittest.TestCase):
     def test_make_simple_base_command(self) -> None:
         unit = BuildozerExecutor(buildozer="foo", buildozer_args=[], workspace=Path(), dry=False)
-        self.assertEqual(unit._base_cmd, ["foo"])
+        self.assertEqual(unit._base_cmd, ["foo"])  # noqa: SLF001
 
     def test_make_complex_base_command(self) -> None:
         unit = BuildozerExecutor(buildozer="foo", buildozer_args=["riff", "raff"], workspace=Path(), dry=True)
-        self.assertEqual(unit._base_cmd, ["foo", "riff", "raff", "-stdout"])
+        self.assertEqual(unit._base_cmd, ["foo", "riff", "raff", "-stdout"])  # noqa: SLF001
 
 
 if __name__ == "__main__":
