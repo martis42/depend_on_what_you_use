fa255a88e82adb48245bce3a920a5fa56a449917
Martin Medler <martis42.dev@gmail.com>
2024-01-14 11:48:03 +0100

    Execute aspect integration tests with macos

--- test/aspect/tree_artifact/test_invalid_tree_artifact.py
+++ test/aspect/tree_artifact/test_invalid_tree_artifact.py
@@ -6,14 +6,12 @@ class TestCase(TestCaseBase):
     def execute_test_logic(self) -> Result:
         expected = ExpectedResult(
             success=False,
-            invalid_includes=[
-                "File='bazel-out/k8-fastbuild/bin/tree_artifact/sources.cc/tree_lib.cc', include='tree_artifact/some_lib.h'"
-            ],
+            # We omit "File='bazel-out/<compilation_mode_and_platform> to allow testing in various environments
+            invalid_includes=["/bin/tree_artifact/sources.cc/tree_lib.cc', include='tree_artifact/some_lib.h'"],
         )
         actual = self._run_dwyu(
             target="//tree_artifact:tree_artifact_library",
             aspect=self.default_aspect,
-            extra_args=["--compilation_mode=fastbuild"],
         )
 
         return self._check_result(actual=actual, expected=expected)
