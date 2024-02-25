942d3b504ba7c9feff3e966fd3639e737d010594
Martin Medler <martis42.dev@gmail.com>
2024-02-25 11:36:43 +0100

    Downgrade rules-python to 0.27.1
    We have no need for a newer version and 0.27.1 is the last version still
    supporting Bazel 5. Thus, we should use 0.27.1 as DWYU is as well still
    supporting Bazel 5.
    While tests where green with 0.28.0, we don't want to risk problems in
    edge cases currently unknown to us.

--- test/apply_fixes/execution_logic.py
+++ test/apply_fixes/execution_logic.py
@@ -35,9 +35,9 @@ load("@bazel_tools//tools/build_defs/repo:utils.bzl", "maybe")
 maybe(
     http_archive,
     name = "rules_python",
-    sha256 = "d70cd72a7a4880f0000a6346253414825c19cdd40a28289bdf67b8e6480edff8",
-    strip_prefix = "rules_python-0.28.0",
-    urls = ["https://github.com/bazelbuild/rules_python/releases/download/0.28.0/rules_python-0.28.0.tar.gz"],
+    sha256 = "e85ae30de33625a63eca7fc40a94fea845e641888e52f32b6beea91e8b1b2793",
+    strip_prefix = "rules_python-0.27.1",
+    urls = ["https://github.com/bazelbuild/rules_python/releases/download/0.27.1/rules_python-0.27.1.tar.gz"],
 )
 
 load("@rules_python//python:repositories.bzl", "python_register_toolchains")
