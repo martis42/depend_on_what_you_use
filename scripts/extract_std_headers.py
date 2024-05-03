b733ec571ff13e1bd6e53e66bb3a683e3223ef8d
Martin Medler <martis42.dev@gmail.com>
2024-05-03 18:39:35 +0200

    Update standard headers
    - Add header introduced by new standards
    - Add all C headers
    - Add all POSIX C headers

--- scripts/extract_std_headers.py
+++ scripts/extract_std_headers.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
 """
-Given a file containing the text of https://en.cppreference.com/w/cpp/header create a list of std headers
+We are interested in the following headers:
+C++     - https://en.cppreference.com/w/cpp/header
+C       - https://en.cppreference.com/w/c/header
+POSIX C - https://en.wikipedia.org/wiki/C_POSIX_library
+
+Go to each site and copy the content into a file. This file then reads the file and prints a list we can use in the
+python file containing the standard header list for DWYU to lookup.
 """
 
 import re
 from pathlib import Path
 
-INPUT_FILE = Path("content_of_cppreference.txt")
-
-with INPUT_FILE.open(encoding="utf-8") as fin:
+with Path("input.txt").open(encoding="utf-8") as fin:
+    headers = []
     for line in fin.readlines():
-        include = re.findall("^<(.+)>$", line)
-        if len(include) == 1:
-            print(f'"{include[0]}",')  # noqa: T201
+        headers.extend(re.findall(r"<([a-z/.]+)>", line))
+    print("\n".join(f'"{h}",' for h in sorted(set(headers))))  # noqa: T201
