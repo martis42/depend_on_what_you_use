456ae547205e4a9b58ea9d923d05cae46ae2b1b7
Martin Medler <martis42.dev@gmail.com>
2024-05-09 23:13:29 +0200

    apply_fixes: More flexible bazel-info usage
    In some workspaces users have to provide custom arguments to use
    'bazel info', which is now possible.

--- test/apply_fixes/tool_cli/test_forward_args_to_buildozer.py
+++ test/apply_fixes/tool_cli/test_forward_args_to_buildozer.py
@@ -11,7 +11,7 @@ class TestCase(TestCaseBase):
         self._create_reports()
         # Make buildozer a noop by writing changes to output instead of to the BUILD file. We don't need to limit the
         # processes for the core test logic. We simply do this to test the forwarding of multiple arguments.
-        self._run_automatic_fix(extra_args=["--fix-unused", "--buildozer-args='-stdout' -P=2"])
+        self._run_automatic_fix(extra_args=["--fix-unused", "--buildozer-arg", "-stdout -P=2"])
 
         target_deps = self._get_target_attribute(target=self.test_target, attribute="deps")
         if (expected := {"//:lib"}) != target_deps:
