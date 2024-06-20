0ca62609fc73367cb1b0cdc5da6bdd314f629278
Martin Medler <martis42.dev@gmail.com>
2024-06-20 22:35:41 +0200

    Fix standard headers list
    Due to an error in the regex parsing the webpage content, all headers
    using `_` were missing.

---
+++ scripts/test/extract_std_headers_test.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import unittest
+
+from scripts.extract_std_headers import extract_header
+
+
+class TestExtractHeader(unittest.TestCase):
+    def test_empty_input_yields_empty_list(self) -> None:
+        self.assertEqual(extract_header(""), [])
+
+    def test_extracting_headers(self) -> None:
+        headers = extract_header(
+            """
+unrelated_stuff
+<foo>
+ignore this
+<bar.h>
+<multiple_header><in_one_line>
+        """.strip()
+        )
+
+        self.assertEqual(headers, ["foo", "bar.h", "multiple_header", "in_one_line"])
+
+
+if __name__ == "__main__":
+    unittest.main()
