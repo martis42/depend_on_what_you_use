dae65bcad1c58178827408788004f3e479c8b43b
Martin Medler <martis42.dev@gmail.com>
2024-07-13 18:21:00 +0200

    Switch to testing rolling instead of specific pre releases
    The downside is that our build can break without changes on our side.
    However, we test the pre releases to be aware of breaking upstream changes
    as early as possible. Thus, this actually more a feature than a problem.

--- test/aspect/version_test.py
+++ test/aspect/version_test.py
@@ -28,6 +28,14 @@ class TestIsCompatibleVersion(unittest.TestCase):
     def test_inside_interval(self) -> None:
         self.assertTrue(CompatibleVersions(minimum="0.9.0", before="1.1.0").is_compatible_to("1.0.0"))
 
+    def test_only_rolling_is_compatible_to_min_version_rolling(self) -> None:
+        self.assertFalse(CompatibleVersions(minimum="rolling").is_compatible_to("999.999.999"))
+        self.assertTrue(CompatibleVersions(minimum="rolling").is_compatible_to("rolling"))
+
+    def test_max_version_rolling_is_compatible_to_all_except_rolling(self) -> None:
+        self.assertTrue(CompatibleVersions(before="rolling").is_compatible_to("999.999.999"))
+        self.assertFalse(CompatibleVersions(before="rolling").is_compatible_to("rolling"))
+
 
 if __name__ == "__main__":
     unittest.main()
