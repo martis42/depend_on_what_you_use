8d9dc7a1171868b9fcd87f507e9c9bf7422af5bf
Martin Medler <martis42.dev@gmail.com>
2024-12-16 09:46:08 +0100

    refactor(test): Use Executor class in apply_fixes integration tests
    Like this there are less variables we have to pass through several functions.

--- test/apply_fixes/execution_logic.py
+++ test/apply_fixes/execution_logic.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import logging
 import subprocess
 import sys
+from dataclasses import dataclass
 from importlib.machinery import SourceFileLoader
 from pathlib import Path, PosixPath
 from platform import system
@@ -22,9 +23,6 @@ sys.path.append(str(WORKSPACE_TEST_DIR))
 # We need to adjust the import path first before performing this import
 # ruff: noqa: E402
 from support.bazel import get_bazel_binary
-
-TEST_CASES_DIR = Path("test/apply_fixes")
-TEST_WORKSPACES_DIR = TEST_CASES_DIR / "workspaces"
 
 MODULE_FILE_TEMPLATE = """
 bazel_dep(name = "depend_on_what_you_use")
@@ -62,110 +60,126 @@ def dwyu_path_as_string(dwyu_path: Path) -> str:
     return str(dwyu_path) if isinstance(dwyu_path, PosixPath) else str(dwyu_path).replace("\\", "\\\\")
 
 
-def setup_test_workspace(
-    origin_workspace: Path, test_sources: Path, extra_workspace_file_content: str, temporary_workspace: Path
-) -> None:
-    copytree(src=test_sources, dst=str(temporary_workspace), dirs_exist_ok=True)
-    with temporary_workspace.joinpath("MODULE.bazel").open(mode="w", encoding="utf-8") as ws_file:
-        ws_file.write(
-            MODULE_FILE_TEMPLATE.format(
-                dwyu_path=dwyu_path_as_string(origin_workspace), extra_content=extra_workspace_file_content
-            )
-        )
-    with temporary_workspace.joinpath(".bazelversion").open(mode="w", encoding="utf-8") as ws_file:
-        ws_file.write(BAZEL_VERSION)
-    with temporary_workspace.joinpath(".bazelrc").open(mode="w", encoding="utf-8") as ws_file:
-        ws_file.write(BAZEL_RC_FILE)
-
-
-def cleanup(test_workspace: Path, bazel_binary: Path) -> None:
-    """
-    Executing a test leaves several traces on the system. For each temporary test workspace an own Bazel server is
-    started and a dedicated output base is created. The Bazel server runs for quite some time before timing out, thus
-    these servers can block a lot of RAM if the tests are run multiple times.
-
-    This function stops the Bazel server and removes the output directory to prevent test runs from cluttering the host
-    system.
-    """
-    process = subprocess.run(
-        [bazel_binary, "info", "output_base"], cwd=test_workspace, check=True, capture_output=True, text=True
-    )
-    output_base = process.stdout.strip()
-
-    # Has to be done before output base cleanup, otherwise the shutdown will create the output base anew
-    subprocess.run([bazel_binary, "shutdown"], cwd=test_workspace, check=True)
-
-    # The hermetic Python toolchain contains read oly files which we can't remove without making them writable
-    subprocess.run(["chmod", "-R", "+rw", output_base], check=True)
-    rmtree(output_base)
-
-
-def execute_test(test: TestCaseBase, origin_workspace: Path, bazel_binary: Path) -> bool:
-    if not test.windows_compatible and system() == "Windows":
-        logging.info(f"--- Skipping Test due to Windows incompatibility '{test.name}'")
-        return True
-
-    logging.info(f">>> Test '{test.name}'")
-    succeeded = False
-    result = None
-
-    with TemporaryDirectory() as temporary_workspace:
-        workspace_path = Path(temporary_workspace)
-        try:
-            setup_test_workspace(
-                origin_workspace=origin_workspace,
-                test_sources=test.test_sources,
-                extra_workspace_file_content=test.extra_workspace_file_content,
-                temporary_workspace=workspace_path,
-            )
-            result = test.execute_test(workspace_path)
-        except Exception:
-            logging.exception("Test failed due to exception:")
-
-        cleanup(test_workspace=workspace_path, bazel_binary=bazel_binary)
-
-    if result is None:
-        result = Error("No result")
-
-    if result.is_success():
-        succeeded = True
-    else:
-        logging.info(result.error)
-
-    logging.info(f'<<< {"OK" if succeeded else "FAILURE"}\n')
-
-    return succeeded
-
-
 def get_current_workspace(bazel_binary: Path) -> Path:
     process = subprocess.run([bazel_binary, "info", "workspace"], check=True, capture_output=True, text=True)
     return Path(process.stdout.strip())
 
 
-def file_to_test_name(test_file: Path) -> str:
-    return test_file.parent.name + "/" + test_file.stem.replace("test_", "", 1)
+@dataclass
+class TestDefinition:
+    name: str
+    script: Path
+    sources: Path
+
+
+class ApplyFixesIntegrationTestsExecutor:
+    def __init__(self, requested_tests: list[str] | None) -> None:
+        self.requested_tests = requested_tests if requested_tests else []
+
+        self.bazel_bin = get_bazel_binary()
+        self.origin_workspace = get_current_workspace(self.bazel_bin)
+        self.test_definitions = self._get_test_definitions()
+
+    def list_tests(self) -> None:
+        logging.info("Available test cases:")
+        logging.info("\n".join(f"- {t.name}" for t in self.test_definitions))
+
+    def execute_tests(self) -> list[str]:
+        """
+        :return: List of failed test cases
+        """
+        tests = []
+        for test in self.test_definitions:
+            if not self.requested_tests or any(requested in test.name for requested in self.requested_tests):
+                module = SourceFileLoader("", str(test.script.resolve())).load_module()
+                tests.append(module.TestCase(name=test.name, test_sources=test.sources, bazel_binary=self.bazel_bin))
+
+        return [test.name for test in tests if not self._execute_test(test)]
+
+    def _execute_test(self, test: TestCaseBase) -> bool:
+        if not test.windows_compatible and system() == "Windows":
+            logging.info(f"--- Skipping Test due to Windows incompatibility '{test.name}'")
+            return True
+
+        logging.info(f">>> Test '{test.name}'")
+        succeeded = False
+        result = None
+
+        with TemporaryDirectory() as temporary_workspace:
+            workspace_path = Path(temporary_workspace)
+            try:
+                self._setup_test_workspace(test=test, test_workspace=workspace_path)
+                result = test.execute_test(workspace_path)
+            except Exception:
+                logging.exception("Test failed due to exception:")
+            self._cleanup(workspace_path)
+
+        if result is None:
+            result = Error("No result")
+        if result.is_success():
+            succeeded = True
+        else:
+            logging.info(result.error)
+        logging.info(f'<<< {"OK" if succeeded else "FAILURE"}\n')
+
+        return succeeded
+
+    def _setup_test_workspace(self, test: TestCaseBase, test_workspace: Path) -> None:
+        copytree(src=test.test_sources, dst=str(test_workspace), dirs_exist_ok=True)
+        with test_workspace.joinpath("MODULE.bazel").open(mode="w", encoding="utf-8") as ws_file:
+            ws_file.write(
+                MODULE_FILE_TEMPLATE.format(
+                    dwyu_path=dwyu_path_as_string(self.origin_workspace),
+                    extra_content=test.extra_workspace_file_content,
+                )
+            )
+        with test_workspace.joinpath(".bazelversion").open(mode="w", encoding="utf-8") as ws_file:
+            ws_file.write(BAZEL_VERSION)
+        with test_workspace.joinpath(".bazelrc").open(mode="w", encoding="utf-8") as ws_file:
+            ws_file.write(BAZEL_RC_FILE)
+
+    def _cleanup(self, test_workspace: Path) -> None:
+        """
+        Executing a test leaves several traces on the system. For each temporary test workspace an own Bazel server is
+        started and a dedicated output base is created. The Bazel server runs for quite some time before timing out, thus
+        these servers can block a lot of RAM if the tests are run multiple times.
+
+        This function stops the Bazel server and removes the output directory to prevent test runs from cluttering the host
+        system.
+        """
+        process = subprocess.run(
+            [self.bazel_bin, "info", "output_base"], cwd=test_workspace, check=True, capture_output=True, text=True
+        )
+        output_base = process.stdout.strip()
+
+        # Has to be done before output base cleanup, otherwise the shutdown will create the output base anew
+        subprocess.run([self.bazel_bin, "shutdown"], cwd=test_workspace, check=True)
+
+        # The hermetic Python toolchain contains read oly files which we can't remove without making them writable
+        subprocess.run(["chmod", "-R", "+rw", output_base], check=True)
+        rmtree(output_base)
+
+    def _get_test_definitions(self) -> list[TestDefinition]:
+        tests_search_dir = self.origin_workspace / "test/apply_fixes"
+        test_scripts = [Path(file) for file in sorted(tests_search_dir.glob("*/test_*.py"))]
+        return [
+            TestDefinition(
+                name=file.parent.name + "/" + file.stem.replace("test_", "", 1),
+                script=file,
+                sources=file.parent / "workspace",
+            )
+            for file in test_scripts
+        ]
 
 
 def main(requested_tests: list[str] | None = None, list_tests: bool = False) -> int:
-    bazel_binary = get_bazel_binary()
-    current_workspace = get_current_workspace(bazel_binary)
-    tests_search_dir = current_workspace / TEST_CASES_DIR
-    test_files = [Path(x) for x in tests_search_dir.glob("*/test_*.py")]
+    executor = ApplyFixesIntegrationTestsExecutor(requested_tests)
 
     if list_tests:
-        test_names = [file_to_test_name(test) for test in test_files]
-        logging.info("Available test cases:")
-        logging.info("\n".join(f"- {t}" for t in sorted(test_names)))
+        executor.list_tests()
         return 0
 
-    tests = []
-    for test in test_files:
-        name = file_to_test_name(test)
-        if (requested_tests and any(requested in name for requested in requested_tests)) or (not requested_tests):
-            module = SourceFileLoader("", str(test.resolve())).load_module()
-            tests.append(module.TestCase(name=name, test_sources=test.parent / "workspace", bazel_binary=bazel_binary))
-
-    failed_tests = [test.name for test in tests if not execute_test(test=test, origin_workspace=current_workspace, bazel_binary=bazel_binary)]
+    failed_tests = executor.execute_tests()
     logging.info(f'Running tests {"FAILED" if failed_tests else "SUCCEEDED"}')
     if failed_tests:
         logging.info("\nFailed tests:")
