75fb0cd647b452e758d7364a9f9601d45129ee1e
Martin Medler <martis42.dev@gmail.com>
2025-01-05 11:25:15 +0100

    test(aspect): Add test for providing defines via cxxopt

---
+++ test/aspect/defines/test_command_line_defines.py
@@ -0,0 +1,26 @@
+from platform import system
+
+from expected_result import ExpectedResult
+from test_case import TestCaseBase
+
+from test.support.result import Result
+
+
+def make_define(flag: str) -> str:
+    """
+    MVSC on Windows has another syntax than gcc and clang
+    """
+    control_character = "/" if system() == "Windows" else "-"
+    return f"{control_character}D{flag}"
+
+
+class TestCase(TestCaseBase):
+    def execute_test_logic(self) -> Result:
+        expected = ExpectedResult(success=True)
+        actual = self._run_dwyu(
+            target="//defines:use_command_line_defines",
+            aspect=self.default_aspect,
+            extra_args=[f"--cxxopt={make_define('SOME_FLAG')}", f"--cxxopt={make_define('SOME_VALUE=42')}"],
+        )
+
+        return self._check_result(actual=actual, expected=expected)
---
+++ test/aspect/defines/use_command_line_defines.h
@@ -0,0 +1,11 @@
+#ifdef SOME_FLAG
+// do some things irrelevant for DWYU
+#else
+#include "not/extsting/header.h"
+#endif
+
+#if SOME_VALUE > 40
+// do some things irrelevant for DWYU
+#else
+#include "not/extsting/header.h"
+#endif
