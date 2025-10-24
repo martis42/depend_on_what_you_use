7db45f7ae01d5dcc2996c0e9f7cfbce463e33832
Martin Medler <36563496+martis42@users.noreply.github.com>
2025-10-24 13:05:33 +0200

    test(benchmark): Streamline benchmarking
    - Report minimum runtime as this is the interesting result. This is how
      fast the machine can run the code. The slower results are due to the
      system being busy with more unrelated tasks
    - Better structured output
    - No need to restrict jobs for the primer targets
    - More complex examples for more computational demand
    - Ensure gtest headers are analyzed a proper C++ standard

--- test/benchmark/lib.py
+++ test/benchmark/lib.py
@@ -25,7 +25,6 @@ def run_benchmark(aspect: str, primer: str, bench_target: str, description: str,
     cmd_base = [
         "bazel",
         "build",
-        *option_jobs,
         # Be sure not be unfair in Python vs precompiled tools
         "--nolegacy_external_runfiles",
         # We do not care about those warnings while benchmarking
@@ -34,11 +33,11 @@ def run_benchmark(aspect: str, primer: str, bench_target: str, description: str,
     ]
     cmd_dwyu = [*cmd_base, f"--aspects=//:aspect.bzl%{aspect}"]
 
-    log.info(f"#### Running Benchmark - {description}")
+    log.info(f"\n#### Running Benchmark - {description}\n")
     times = []
     log.debug(f"Benchmarking cmd: {shlex.join(cmd_dwyu)}\n")
-    for n in range(iterations):
-        log.info(f">> Iteration {n}")
+    for n in range(1, iterations + 1):
+        log.info(f"\n>> Iteration {n}")
 
         # Make sure no cached results are used
         subprocess.run(["bazel", "clean"], shell=False, check=True, capture_output=capture_output)
@@ -48,12 +47,17 @@ def run_benchmark(aspect: str, primer: str, bench_target: str, description: str,
 
         # Run benchmark command
         start = time()
-        subprocess.run([*cmd_dwyu, "--", bench_target], shell=False, check=True, capture_output=capture_output)
+        subprocess.run(
+            [*cmd_dwyu, *option_jobs, "--", bench_target], shell=False, check=True, capture_output=capture_output
+        )
         duration = time() - start
         times.append(duration)
-        log.info(f">> Duration {duration:.3f} [s]\n")
+        log.info(f">> Duration {duration:.3f} [s]")
 
-    log.info("Summary")
+    log.info("\nSummary")
     average_time = sum(times) / len(times)
-    log.info(f"  Average runtime    : {average_time:.3f} [s]")
-    log.info(f"  Standard deviation : {stdev(times):.3f} [s]\n\n")
+    log.info(f"  Minimum runtime    : {min(times):.3f} [s]")
+    if len(times) > 1:
+        log.info(f"  Average runtime    : {average_time:.3f} [s]")
+        log.info(f"  Standard deviation : {stdev(times):.3f} [s]")
+    log.info("")
