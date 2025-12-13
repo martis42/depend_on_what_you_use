ca9d3dc0b78af55aff58d935d36107ad21d18050
Martin Medler <36563496+martis42@users.noreply.github.com>
2025-12-13 10:29:17 +0100

    feat(aspect): Make the C++ impl preprocessing more robust

---
+++ test/aspect/preprocessing/use_complex_libraries.cpp
@@ -0,0 +1,8 @@
+// Include headers from real projects which are known not to be trivial to preprocess
+#include <boost/chrono.hpp>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+int main() {
+    return 0;
+}
