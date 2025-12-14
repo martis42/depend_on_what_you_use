21bdbbbd89f1dbc81686dda75a0a2672cdc67a8e
Martin Medler <36563496+martis42@users.noreply.github.com>
2025-12-14 10:53:47 +0100

    refactor(aspect): Rely on `external_includes` being available
    Now that our minimum required Bazel version is > 7.0.0 we no longer have
    to check if `external_includes` is even available.

--- test/aspect/complex_includes/test_feature_external_include_paths.py
+++ test/aspect/complex_includes/test_feature_external_include_paths.py
@@ -1,18 +1,10 @@
 from expected_result import ExpectedResult
-from test_case import CompatibleVersions, TestCaseBase
+from test_case import TestCaseBase
 
 from test.support.result import Result
 
 
 class TestCase(TestCaseBase):
-    @property
-    def compatible_bazel_versions(self) -> CompatibleVersions:
-        """
-        The 'external_include_paths' feature exists for Bazel < 7.0.0. But the corresponding information was not added
-        to CompilationContext, which hides it from an aspect. This information was added in Bazel 7.0.0.
-        """
-        return CompatibleVersions(minimum="7.0.0")
-
     def execute_test_logic(self) -> Result:
         """
         The 'external_include_paths' toolchain feature automatically moves some include paths in the Bazel
