ed8c6f1e2ed1bb0dbf24fe97cdd4b2c1172f548d
Martin Medler <36563496+martis42@users.noreply.github.com>
2025-12-27 08:56:21 +0100

    build: Harden C++11 compliance

---
+++ scripts/ensure_cpp11_compatibility.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+# Due to using warning '-Wc++11-compat' we are confident our own code has C++11 compatibility.
+# However, since we ignore warnings from third_party code, we need to make sure that the third_party code is also C++11 compatible.
+
+TARGETS=(
+    "//dwyu/aspect/private/preprocessing:main"
+    "//dwyu/aspect/private/preprocessing:main_no_preprocessing"
+    "//dwyu/aspect/private/process_target:main_cc"
+)
+
+echo ""
+echo ">> Ensuring the following targets compile with C++11:"
+for target in "${TARGETS[@]}"; do
+    echo ">>   ${target}"
+done
+echo ""
+
+bazel build --cxxopt=-std=c++11 --host_cxxopt=-std=c++11 -- ${TARGETS[@]}
