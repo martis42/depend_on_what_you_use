0722735af3fe7b7fa7814d5069363e3359be0c6d
Martin Medler <36563496+martis42@users.noreply.github.com>
2026-01-18 11:49:19 +0100

    style: Fix typo

--- test/aspect/skip_external_targets/test_skip_broken_target.py
+++ test/aspect/skip_external_targets/test_skip_broken_target.py
@@ -6,7 +6,7 @@ from test.support.result import Result
 
 class TestCase(TestCaseBase):
     def execute_test_logic(self) -> Result:
-        # If we would not skip al external targets the analysis would find an issue with the broken dependency
+        # If we would not skip all external targets the analysis would find an issue with the broken dependency
         expected = ExpectedResult(success=True)
         actual = self._run_dwyu(
             target="@skip_external_deps_test_repo//:broken_dep",
