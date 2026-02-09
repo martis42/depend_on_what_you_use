1c0f56fe4849aed111d6b334c91d0749ee299a22
Martin Medler <36563496+martis42@users.noreply.github.com>
2026-02-09 13:09:58 +0100

    fix test

--- test/apply_fixes/missing_dependency/workspace/use_manipulated_bar.h
+++ test/apply_fixes/missing_dependency/workspace/use_manipulated_bar.h
@@ -1,7 +1,7 @@
 #ifndef MISSING_DEPENDENCY_WORKSPACE_USE_MANIPULATED_BAR_H
 #define MISSING_DEPENDENCY_WORKSPACE_USE_MANIPULATED_BAR_H
 
-#include "missing_dependency/workspace/new/path/sub/bar.h"
+#include "new/path/sub/bar.h"
 
 int useBar() {
     return doBar();
