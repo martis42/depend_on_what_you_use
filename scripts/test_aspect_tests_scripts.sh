6d3ac286873488b9dc96573b2ac26662d3a73834
Martin Medler <36563496+martis42@users.noreply.github.com>
2026-04-04 09:29:55 +0200

    chore: Move test scripts into script dir
    The test test scripts do not belong into the root directory. Also make it
    possible to execute the script from other locations than the workspace
    root.
    Additionally, it turned out we still tried to test the legacy Python impl,
    which we clean up now.

--- scripts/test_aspect_tests_scripts.sh
+++ scripts/test_aspect_tests_scripts.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 set -o errexit
+set -o nounset
 
-cd test/aspect
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+cd "$SCRIPT_DIR"/../test/aspect
 bazel test //:all
 cd -
