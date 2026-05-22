313e3c33cd89a1c7385e9718fa068ea5ca3e0f27
Martin Medler <36563496+martis42@users.noreply.github.com>
2026-05-22 09:10:31 +0200

    chore: Use consistent Bazel version style for testing
    We used `9.*` during the Bazel 9 release phase. But in general, we only
    test against released Bazel versions.

--- test/workspace_integration/test.py
+++ test/workspace_integration/test.py
@@ -19,12 +19,12 @@ from test.support.bazel import get_bazel_binary, get_explicit_bazel_version, mak
 logging.basicConfig(format="%(message)s", level=logging.INFO)
 log = logging.getLogger()
 
-# Kep in sync with: MODULE.bazel, README.md, .bcr/presubmit.yml and test/aspect/execute_tests.py
+# Keep in sync with: MODULE.bazel, README.md, .bcr/presubmit.yml and test/aspect/execute_tests.py
 BAZEL_VERSIONS_UNDER_TEST = [
     "7.6.0",
     "7.x",
     "8.x",
-    "9.*",
+    "9.x",
     "rolling",
 ]
 
