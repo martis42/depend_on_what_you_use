cc60f72b92b04e3494bba217235f20ce52a6f8f4
Alexander Scott <alexanderscott@outlook.com>
2026-08-03 13:41:11 +1000

    Run pre-commit

--- test/aspect/skip_targets/test_non_matching_target_is_analyzed.py
+++ test/aspect/skip_targets/test_non_matching_target_is_analyzed.py
@@ -7,7 +7,9 @@ from test.support.result import Result
 class TestCase(TestCaseBase):
     def execute_test_logic(self) -> Result:
         expected = ExpectedFailure(
-            ExpectedDwyuFailure(target="//skip_targets:broken_sibling", unused_public_deps=["//skip_targets:unused_lib"])
+            ExpectedDwyuFailure(
+                target="//skip_targets:broken_sibling", unused_public_deps=["//skip_targets:unused_lib"]
+            )
         )
         actual = self._run_dwyu(
             target="//skip_targets:broken_sibling",
